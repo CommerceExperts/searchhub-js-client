38cb029668c96de20226223401814fadf3e83969
Tobias Armbruster <armbruster.tobias@gmx.de>
2024-09-05 15:00:13 +0200

    export cache interface + class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export {SmartQueryClient, SmartQueryClientConfig} from "./SmartQueryClient";
 export {CookieAccess, BrowserCookieAccess, getBrowserCookie, setBrowserCookie, SEARCH_COLLECTOR_SESSION_COOKIE_NAME} from "./browser/cookies";
 export {AbTestSegment, generateRandomSegment, AbTestSegmentManager, AbTestSegmentManagerConfig} from "./AbTestSegmentManager";
+export {SmartQueryClient, SmartQueryClientConfig} from "./SmartQueryClient";
 export {Suggestion, SmartSuggestClientConfig, SmartSuggestClient} from "./SmartSuggestClient";
+export {Cache} from "cache/Cache";
+export {SimpleMemoryCache} from "cache/SimpleMemoryCache";
